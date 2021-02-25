ea3628513b25b21447f671302d499c8b41494969
Abner Rodrigues <abnerluis1001@gmail.com>
2021-02-24 21:35:16 -0300

    fix: fixed footer position

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,17 @@
 import styled from 'styled-components'
 
 const StyledFooter = styled.footer`
-  position: fixed;
-  bottom: 0;
-  width: 100%;
+  position: absolute;
+  right: 0;
   font-family: 'Inconsolata', monospace;
-  font-size: 0.425rem;
+  font-size: 12px;
   font-weight: 700;
   letter-spacing: -0.05em;
   color: ${({ theme }) => theme.colors.black};
 
   p {
     padding: 0.688rem;
-    float: right;
+    text-align: end;
   }
 `
 
