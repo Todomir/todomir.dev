746ee8b5b7e0d5a558fae8b1b7f5d0a1c8ffaed4
Abner Rodrigues <abnerluis1001@gmail.com>
2021-02-25 21:32:54 -0300

    feat(_app): change const to function and prepared to page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
+import { AnimatePresence } from 'framer-motion'
 import { ThemeProvider } from 'styled-components'
 
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
 
-const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
+export default function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
       <Head>
         <title>Abner Rodrigues | Fullstack Developer</title>
       </Head>
-      <Component {...pageProps} />
+      <AnimatePresence exitBeforeEnter>
+        <Component {...pageProps} key={router.route} />
+      </AnimatePresence>
       <GlobalStyle />
     </ThemeProvider>
   )
 }
-
-export default MyApp
