940d9a42f380d0cbb90ca26069901afeafd09177
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 11:47:27 -0300

    refactor(styles/theme): change breakpoints back to object syntax
    Change theme breakpoints styles from an array syntax back to an object syntax
    
    BREAKING CHANGE: Theme breakpoint styles aren't an array anymore, refer to it as an object

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -7,7 +7,14 @@ const theme = {
     black: '#09132D',
     white: '#FFF'
   },
-  breakpoints: [0, 640, 768, 1024, 1280, 1920]
+  breakpoints: {
+    xs: '0px',
+    sm: '640px',
+    md: '768px',
+    lg: '1024px',
+    xl: '1280px',
+    '2xl': '1920px'
+  }
 }
 
 export default theme
