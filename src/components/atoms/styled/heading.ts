8a768964545c3bb6ddafdd1402b8b96db2355042
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 11:29:19 -0300

    feat(atoms): create Heading component

---
+++ src/components/atoms/styled/heading.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Heading = styled.h1`
+  font-variation-settings: 'wght' 640;
+  font-size: 4.236rem;
+  line-height: 110%;
+`
+
+export default Heading
