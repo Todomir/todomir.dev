afb9e863e900f2b32ff83aefa1175ad3c61a7a7c
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 12:09:07 -0300

    feat(functions): create a clamp generator function for linearly scaling

---
+++ src/utils/functions/clamp.ts
@@ -0,0 +1,15 @@
+export default (minWidthPx: number, maxWidthPx: number) =>
+  (minFontSize: number, maxFontSize: number) => {
+    const root = document.querySelector('html')
+    const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2))
+
+    const minWidth = minWidthPx / pixelsPerRem
+    const maxWidth = maxWidthPx / pixelsPerRem
+
+    const slope = (maxFontSize - minFontSize) / (maxWidth - minWidth)
+    const yAxisIntersection = -minWidth * slope + minFontSize
+
+    return `clamp( ${minFontSize}rem, ${yAxisIntersection}rem + ${
+      slope * 100
+    }vw, ${maxFontSize}rem )`
+  }
