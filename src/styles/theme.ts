e73e10732b42baca67f033c2b9447dfe9fdd6b47
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 13:23:10 -0300

    feat(styles/theme): add more colors

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,8 @@
 const theme = {
   colors: {
     green: '#37ECBA',
+    'light-green': '#DBFFED',
+    'dark-green': '#00E593',
     blue: '#8ED6F2',
     purple: '#D0C2FC',
     orange: '#E27018',
