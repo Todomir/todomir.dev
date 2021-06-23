3e1da970d522225e3e749d6acb204c291f6a6a84
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 12:11:13 -0300

    feat(styles/theme): change breakpoints values from strings to numbers
    Clamp function requires numbers to function, so the breakpoints was converted to numbers. Their
    values are still measured on pixels.
    
    BREAKING CHANGE: Theme breakpoints changed from strings to numbers

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,12 +8,12 @@ const theme = {
     white: '#FFF'
   },
   breakpoints: {
-    xs: '0px',
-    sm: '640px',
-    md: '768px',
-    lg: '1024px',
-    xl: '1280px',
-    '2xl': '1920px'
+    xs: 0,
+    sm: 640,
+    md: 768,
+    lg: 1024,
+    xl: 1280,
+    '2xl': 1920
   }
 }
 
