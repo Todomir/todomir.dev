2d482125e5148beb4cba29c236ef186317c30f0f
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 20:59:30 -0300

    feat(atoms): export Container and Text

--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,3 +1,5 @@
 export { default as Button } from './styled/button'
+export { default as Container } from './styled/container'
 export { default as Heading } from './styled/heading'
 export { default as Highlight } from './styled/highlight'
+export { default as Text } from './styled/text'
