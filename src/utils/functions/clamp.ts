5f6ecb9cc96d32e86a7760ca8209a68a0c1e81f0
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 13:21:28 -0300

    feat(functions/clamp): adapt to work with ssr

--- src/utils/functions/clamp.ts
+++ src/utils/functions/clamp.ts
@@ -1,5 +1,7 @@
 export default (minWidthPx: number, maxWidthPx: number) =>
   (minFontSize: number, maxFontSize: number) => {
+    if (typeof document === 'undefined') return
+
     const root = document.querySelector('html')
     const pixelsPerRem = Number(getComputedStyle(root).fontSize.slice(0, -2))
 
