7dcb09599b1b1e08032a3ff19c75098103f38b60
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-24 20:45:46 -0300

    feat(atoms/container): edit values for container size and span full class

--- src/components/atoms/styled/container.ts
+++ src/components/atoms/styled/container.ts
@@ -2,14 +2,14 @@ import styled from 'styled-components'
 
 const Container = styled.section`
   display: grid;
-  grid-template-columns: 1fr min(100ch, calc(100% - 64px)) 1fr;
+  grid-template-columns: 1fr min(92ch, calc(100% - 64px)) 1fr;
   grid-column-gap: 32px;
 
   & > * {
     grid-column: 2;
   }
 
-  &[data-fw] {
+  .span-full {
     width: 100%;
     grid-column: 1 / -1;
   }
