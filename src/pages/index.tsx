a0d74f9c9dd1c335881ad6701742273e4d7b108e
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-24 20:47:51 -0300

    feat(pages/index): add Navbar and Problem section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
-import { Container } from '@components/atoms'
-import { Hero } from '@components/layout'
+import { Container, Navbar } from '@components/atoms'
+import { Hero, Problem } from '@components/layout'
 
 export default function Home() {
   return (
     <Container as="main">
+      <Navbar />
       <Hero />
+      <Problem />
     </Container>
   )
 }
