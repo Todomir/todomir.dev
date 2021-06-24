9a88cec96c1d02c4f0b5c8c68e464386c1306f23
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-23 22:00:35 -0300

    feat(style/theme): modularize theme object

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,10 @@
+export default {
+  green: '#37ECBA',
+  'light-green': '#DBFFED',
+  'dark-green': '#00E593',
+  blue: '#8ED6F2',
+  purple: '#D0C2FC',
+  orange: '#E27018',
+  black: '#09132D',
+  white: '#FFF'
+}
---
+++ src/styles/theme/index.ts
@@ -0,0 +1,11 @@
+import breakpoints from './breakpoints'
+import colors from './colors'
+import fonts from './fonts'
+
+const theme = {
+  colors,
+  fonts,
+  breakpoints
+}
+
+export default theme
