077d277fd1fffbf041f77b745c4606c691a95f63
Abner Rodrigues <abnerluis1001@gmail.com>
2021-06-25 20:53:52 -0300

    fix(atoms/navbar): fix navbar button size

--- src/components/atoms/navbar/styles.ts
+++ src/components/atoms/navbar/styles.ts
@@ -1,7 +1,11 @@
+import { Button as NavButton } from '@components/atoms'
+
 import styled from 'styled-components'
 
 export const Nav = styled.nav`
   ul {
+    padding-inline-start: 0;
+
     display: flex;
     list-style-type: none;
     align-items: center;
@@ -9,5 +13,22 @@ export const Nav = styled.nav`
 
     margin-block-start: 2em;
     margin-block-end: 2em;
+
+    li {
+      ${NavButton} {
+        --tt-key: nav-button;
+        font-size: var(--fs-body);
+
+        @keyframes nav-button {
+          0%,
+          25.874999999999996% {
+            font-size: 0.618rem;
+          }
+          48% {
+            font-size: 1rem;
+          }
+        }
+      }
+    }
   }
 `
