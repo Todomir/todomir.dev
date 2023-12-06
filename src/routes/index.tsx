1989aa491d935c5f83a3ddd79b509f4ca83155b6
Abner Rodrigues <abnerluis1001@gmail.com>
2023-12-05 21:49:47 -0300

    chore: readd components to home

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,21 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
+import About from "~/features/home/about";
+import Hero from "~/features/home/hero";
+import Quote from "~/features/home/quote";
+
 export default component$(() => {
-	return <h1>hello from qwik</h1>;
+	return (
+		<>
+			<Hero />
+			<Quote />
+			<div class="px-5 md:px-20">
+				<hr class="w-full border-zinc-700" />
+			</div>
+			<About />
+		</>
+	);
 });
 
 export const head: DocumentHead = {
