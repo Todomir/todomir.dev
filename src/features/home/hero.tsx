35a0bedaa77a78ff5c95b4bee285a27569130ec8
Abner Rodrigues <abnerluis1001@gmail.com>
2023-12-18 14:37:10 -0300

    feat(features): add hero images animation

--- src/features/home/hero.tsx
+++ src/features/home/hero.tsx
@@ -1,9 +1,52 @@
 import { component$, useVisibleTask$ } from "@builder.io/qwik";
-import { animate, stagger } from "motion";
+import { animate, spring, stagger } from "motion";
 
 import IconArrowDown from "~/media/icons/arrow/down.svg?jsx";
 
 export default component$(() => {
+	useVisibleTask$(() => {
+		const aside = document.querySelector<HTMLDivElement>("#hero aside");
+		if (!aside) return;
+
+		const leftImages = aside.querySelectorAll<HTMLDivElement>(
+			"[data-direction=left]",
+		);
+		const rightImages = aside.querySelectorAll<HTMLDivElement>(
+			"[data-direction=right]",
+		);
+
+		if (!leftImages.length || !rightImages.length) return;
+
+		const springConfig = {
+			mass: 5,
+			stiffness: 200,
+			damping: 150,
+		};
+
+		animate(
+			leftImages,
+			{
+				x: ["-80%", 0],
+				opacity: [0, 1],
+			},
+			{
+				delay: stagger(0.1),
+				easing: spring(springConfig),
+			},
+		);
+
+		animate(
+			rightImages,
+			{
+				x: ["50%", 0],
+				opacity: [0, 1],
+			},
+			{
+				delay: stagger(0.4),
+				easing: spring(springConfig),
+			},
+		);
+	});
 	return (
 		<section
 			id="hero"
@@ -12,22 +55,22 @@ export default component$(() => {
 			<aside class="overflow-x-clip absolute full-width -translate-y-12 pointer-events-none inset-0 mb-12">
 				<div
 					data-direction="left"
-					class="absolute -top-12 -left-24 aspect-video w-[min(60%,463px)] bg-zinc-900 rounded-3xl shadow-2xl"
+					class="absolute -top-12 -left-24 aspect-video w-[min(60%,463px)] bg-zinc-900 rounded-3xl shadow-2xl opacity-0"
 				/>
 
 				<div
 					data-direction="left"
-					class="absolute -bottom-4 -left-36 aspect-video w-[min(60%,463px)] bg-zinc-900 rounded-3xl shadow-2xl"
+					class="absolute -bottom-4 -left-36 aspect-video w-[min(60%,463px)] bg-zinc-900 rounded-3xl shadow-2xl opacity-0"
 				/>
 
 				<div
 					data-direction="right"
-					class="absolute -bottom-8 -right-24 aspect-video w-[min(60%,463px)] bg-zinc-900 rounded-3xl shadow-2xl"
+					class="absolute -bottom-8 -right-24 aspect-video w-[min(60%,463px)] bg-zinc-900 rounded-3xl shadow-2xl opacity-0"
 				/>
 
 				<div
 					data-direction="right"
-					class="absolute -top-24 -right-24 aspect-video w-[min(60%,318px)] bg-zinc-900 rounded-3xl shadow-2xl"
+					class="absolute -top-24 -right-24 aspect-video w-[min(60%,318px)] bg-zinc-900 rounded-3xl shadow-2xl opacity-0"
 				/>
 			</aside>
 
