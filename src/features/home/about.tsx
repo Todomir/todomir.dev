884b9d067dbb98b15af08fe357a67e729f0e6e42
Abner Rodrigues <abnerluis1001@gmail.com>
2023-12-19 10:05:41 -0300

    feat(features/home): improve styles on about and quote sections

--- src/features/home/about.tsx
+++ src/features/home/about.tsx
@@ -6,21 +6,21 @@ export default component$(() => {
 	return (
 		<section
 			id="about"
-			class="full-width bg-zinc-950 flex flex-col px-16 py-20 md:py-60 max-md:px-5"
+			class="full-width bg-zinc-950 md:px-16 py-20 md:py-60 px-5"
 		>
 			<div class="flex flex-col lg:flex-row md:gap-20">
 				<ImgMe class="ml-0 object-cover rounded-lg object-center w-full overflow-hidden grow max-h-[600px] mt-10" />
-				<div class="flex flex-col my-auto max-md:max-w-full max-md:mt-10">
-					<h2 class="text-gray-100 text-5xl font-medium leading-[59px] tracking-tighter max-md:max-w-full max-md:text-4xl max-md:leading-[54px]">
+				<div class="flex flex-col my-auto max-w-full mt-10">
+					<h2 class="text-gray-100 md:text-5xl font-medium md:leading-[59px] tracking-tighter max-w-full text-4xl leading-[54px]">
 						I believe in empowering <em class="font-serif italic">people</em>
 					</h2>
-					<h3 class="text-zinc-500 text-2xl leading-8 tracking-tight mt-6 max-md:max-w-full">
+					<h3 class="text-zinc-500 text-2xl leading-8 tracking-tight mt-6 max-w-full">
 						<span class="font-serif italic">People</span> are at the heart of
 						any business. I develop software that brings{" "}
 						<span class="font-serif italic">people</span> together and empowers
 						them, all while bringing a smile to their faces.
 					</h3>
-					<p class="text-gray-100 text-base leading-6 tracking-normal mt-12 max-md:max-w-full mb-6">
+					<p class="text-gray-100 text-base leading-6 tracking-normal mt-12 max-w-full mb-6">
 						I'm Abner 👋, an innovative and passionate Web Developer with a
 						flair for creating elegant solutions in the least amount of time.
 					</p>
@@ -34,7 +34,7 @@ export default component$(() => {
 						href="mailto:abnerluis1001@gmail.com"
 						target="_blank"
 						rel="noreferrer"
-						class="text-zinc-500 text-2xl leading-8 tracking-tight whitespace-nowrap underline cursor-pointer mt-12 max-md:max-w-full"
+						class="text-zinc-500 text-2xl leading-8 tracking-tight whitespace-nowrap underline cursor-pointer mt-12 max-w-full"
 					>
 						abnerluis1001@gmail.com
 					</a>
