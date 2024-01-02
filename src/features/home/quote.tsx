5481563ce83394f70b71dd0b06b438700d2d3348
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-02 11:06:06 -0300

    fix(features/home): fix style conflict on  quote components

--- src/features/home/quote.tsx
+++ src/features/home/quote.tsx
@@ -7,14 +7,15 @@ import { EMOJIS } from "~/utils/constants";
 export default component$(() => {
   return (
     <div class="full-width mx-auto w-full bg-zinc-950 px-16 py-10 pl-5 pr-8 text-center text-xl md:pb-32 md:pt-52 md:text-3xl lg:pb-60">
-      <p class="text-balance w-full text-zinc-500">
+      <p class="w-full text-balance text-zinc-500">
         <span
           dangerouslySetInnerHTML={$localize`A <span class="text-zinc-200">creative front-end engineer</span> with a passion for crafting`}
         />{" "}
         <span class="text-zinc-200">
           <Sparkles>{$localize`visually stunning`}</Sparkles>{" "}
-          <CharacterButton characters={EMOJIS}>{$localize`interactive web experiences.`}</CharacterButton>
+          <CharacterButton characters={EMOJIS}>{$localize`interactive`}</CharacterButton>
         </span>{" "}
+        {$localize`web experiences.`}{" "}
         <span
           dangerouslySetInnerHTML={$localize`Specializing in <span class="text-zinc-200"> translating design concepts into efficient and <em class="font-serif italic">elegant</em> user interfaces,</span> with a keen <span class="text-zinc-200">eye for detail</span> and a focus on <span class="text-zinc-200">user-centric design principle</span>.`}
         />
