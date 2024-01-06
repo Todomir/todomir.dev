ccf79dc63ed7d12b7af1672b2ff68026340a7c0b
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-06 10:04:25 -0300

    chore: remove static adapter

--- adapters/cloudflare-pages/vite.config.ts
+++ adapters/cloudflare-pages/vite.config.ts
@@ -10,6 +10,8 @@ export default extendConfig(baseConfig, () => {
         input: ["src/entry.cloudflare-pages.tsx", "@qwik-city-plan"],
       },
     },
-    plugins: [cloudflarePagesAdapter()],
+    plugins: [
+      cloudflarePagesAdapter(),
+    ],
   };
 });
