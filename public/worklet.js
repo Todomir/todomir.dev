9fb3a9bcc1c21b8d67d01bfe22c3b33ee6a9e62f
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-09 14:13:14 -0300

    chore: delete unused code

--- public/worklet.js
+++
@@ -1,7 +0,0 @@
-(async function () {
-  if (!("paintWorklet" in CSS)) {
-    await import("https://unpkg.com/css-paint-polyfill");
-  }
-
-  CSS.paintWorklet.addModule("/border.js");
-})();
--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -17,8 +17,6 @@ export const RouterHead = component$(() => {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
-      <script async src="/worklet.js" />
-
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
       ))}
