21dbdb32e0ad17a8c66a23d08b7e89033749b473
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-09 16:02:57 -0300

    fix(change-locale): fix wrong localization when on blog post

--- src/components/change-locale/change-locale.tsx
+++ src/components/change-locale/change-locale.tsx
@@ -1,8 +1,6 @@
 import { component$ } from "@builder.io/qwik";
-import { useLocation } from "@builder.io/qwik-city";
 import {
   inlineTranslate,
-  localizePath,
   useDisplayName,
   useSpeakConfig,
   useSpeakLocale,
@@ -11,13 +9,9 @@ import {
 export default component$(() => {
   const t = inlineTranslate();
 
-  const pathname = useLocation().url.pathname;
-
   const locale = useSpeakLocale();
   const config = useSpeakConfig();
   const dn = useDisplayName();
-
-  const getPath = localizePath();
 
   return (
     <>
@@ -30,7 +24,7 @@ export default component$(() => {
             "text-sm block leading-5 tracking-normal text-zinc-500 underline underline-offset-2 transition-all ease-spring-2 hover:font-semibold hover:text-zinc-100",
             { "font-bold text-zinc-300": value.lang == locale.lang },
           ]}
-          href={getPath(pathname, value.lang)}
+          href={`/${value.lang}`}
         >
           {dn(value.lang, { type: "language" })}
         </a>
