1a462b23a21411ce7c91d644b90a88f91a10d404
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-10 15:49:59 -0300

    i18n: validate locale before setting

--- src/routes/plugin.ts
+++ src/routes/plugin.ts
@@ -2,7 +2,6 @@ import type { RequestHandler } from "@builder.io/qwik-city";
 import { validateLocale } from "qwik-speak";
 
 import { config } from "../speak.config";
-//import { rewriteRoutes } from '../speak-routes';
 
 export const onRequest: RequestHandler = ({
   params,
@@ -10,48 +9,25 @@ export const onRequest: RequestHandler = ({
   error,
   request,
 }) => {
-  let lang: string | undefined = undefined;
+  const acceptLanguage = request.headers.get("accept-language");
+  let userLang = acceptLanguage?.split(";")[0]?.split(",")[0];
+  // Remove -US from en-US
+  userLang = userLang === "en-US" ? "en" : userLang;
 
-  if (params.lang && validateLocale(params.lang)) {
-    // Check supported locales
-    lang = config.supportedLocales.find((value) => value.lang === params.lang)
-      ?.lang;
-    // 404 error page
-    if (!lang) throw error(404, "Page not found");
-  } else {
-    const acceptLanguage = request.headers.get("accept-language");
-    // Try to use user language
-    if (acceptLanguage) {
-      lang = acceptLanguage.split(";")[0]?.split(",")[0];
-      // Remove -US from en-US
-      if (lang === "en-US") lang = "en";
-    } else {
-      lang = config.defaultLocale.lang;
-    }
-  }
+  const langParam =
+    params.lang && validateLocale(params.lang) ? params.lang : undefined;
+  const langUser = userLang && validateLocale(userLang) ? userLang : undefined;
+
+  const lang = langParam || langUser || config.defaultLocale.lang;
+
+  // Check supported locales
+  const isSupportedLocale = config.supportedLocales.some(
+    (value) => value.lang === lang,
+  );
+
+  // 404 error page
+  if (!isSupportedLocale) throw error(404, "Page not found");
 
   // Set Qwik locale
   locale(lang);
 };
-
-/**
- * Uncomment this lines to use url rewriting to translate paths.
- * Remove [..lang] from folders structure
- */
-// export const onRequest: RequestHandler = ({ locale, error, url }) => {
-//   let lang: string | undefined = undefined;
-
-//   const prefix = extractFromUrl(url);
-
-//   if (prefix && validateLocale(prefix)) {
-//     // Check supported locales
-//     lang = config.supportedLocales.find(value => value.lang === prefix)?.lang;
-//     // 404 error page
-//     if (!lang) throw error(404, 'Page not found');
-//   } else {
-//     lang = config.defaultLocale.lang;
-//   }
-
-//   // Set Qwik locale
-//   locale(lang);
-// };
