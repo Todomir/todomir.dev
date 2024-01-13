020241d87c864600faf0f1b762673f9fef4c46a5
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-13 13:20:24 -0300

    fix(features/home): fix height change on mobile

--- src/features/home/quote.tsx
+++ src/features/home/quote.tsx
@@ -8,7 +8,7 @@ import { EMOJIS } from "~/utils/constants";
 export default component$(() => {
   const t = inlineTranslate();
   return (
-    <div class="full-width mx-auto h-[50vh] w-full bg-zinc-950 px-16 py-10 pl-5 pr-8 text-center text-xl md:pb-32 md:pt-52 md:text-3xl lg:pb-60">
+    <div class="full-width mx-auto h-screen max-h-[50rem] w-full bg-zinc-950 px-16 py-10 pl-5 pr-8 text-center text-xl md:pb-32 md:pt-52 md:text-3xl lg:pb-60">
       <p class="m-auto h-fit w-full text-balance text-zinc-500">
         <span dangerouslySetInnerHTML={t("home.quote.description")} />{" "}
         <span class="text-zinc-200">
