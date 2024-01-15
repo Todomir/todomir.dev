e4ab50c171d8416c8df94e64de3289c4687afe4b
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-15 12:16:33 -0300

    refactor(about): tweak layout (again)

--- src/routes/about/index.tsx
+++ src/routes/about/index.tsx
@@ -14,51 +14,58 @@ export default component$(() => {
   return (
     <section class="full-width content-grid h-full w-full bg-zinc-950 pt-48 text-zinc-300">
       <div class="grid grid-cols-1 pt-10 md:grid-cols-2 md:gap-16">
-        <header class="col-span-2 grid grid-cols-subgrid">
-          <div class="mb-16 md:col-start-1">
-            <a
-              class="inline-flex items-center gap-3 font-light tracking-tight text-white"
-              href="/"
-            >
-              /home
-            </a>
-            <h1
-              class="my-8 text-balance text-4xl font-medium tracking-tighter text-white lg:text-6xl"
-              dangerouslySetInnerHTML={t("home.about.title")}
-            />
-            <h2 class="text-xl">{t("home.about.presentation")}</h2>
-            <p class="mt-6 text-base leading-8 text-zinc-400">
-              {t("home.about.description")}
-            </p>
-          </div>
+        <header class="col-span-full grid grid-cols-subgrid grid-rows-[repeat(5,auto)]">
+          <a
+            class="inline-flex items-center gap-3 font-light tracking-tight text-white"
+            href="/"
+          >
+            /home
+          </a>
+          <h1
+            class="col-start-1 my-8 text-balance text-4xl font-medium tracking-tighter text-white lg:text-6xl"
+            dangerouslySetInnerHTML={t("home.about.title")}
+          />
+          <h2 class="col-start-1 text-xl">{t("home.about.presentation")}</h2>
+          <p class="col-start-1 mb-12 mt-6 text-base leading-8 text-zinc-400">
+            {t("home.about.description")}
+          </p>
 
           <ImgMe
-            class="col-start-1 h-auto w-full object-cover md:col-start-2"
+            class="md:col-start-2 md:row-span-full"
             alt={t("home.about.meImageAlt")}
           />
         </header>
 
-        <hr class="col-span-2 mb-12 mt-8 border-b border-zinc-700" />
+        <hr class="col-span-full mb-64 mt-8 border-b border-zinc-700" />
 
-        <div class="sticky top-0 col-span-2 mt-auto grid grid-cols-subgrid space-y-12 text-pretty pt-12 md:mb-64">
-          <p class="col-start-1" dangerouslySetInnerHTML={t("about.intro")} />
-          <p class="col-start-1" dangerouslySetInnerHTML={t("about.tcc")} />
-
-          <p class="col-start-1" dangerouslySetInnerHTML={t("about.jsp")} />
-
+        <div class="col-span-full grid grid-cols-subgrid grid-rows-[repeat(5,auto)]">
           <ImgMe2
-            class="col-start-1 aspect-[3/5] h-full w-full object-cover md:absolute md:col-start-2"
+            class="mb-24 md:sticky md:top-64 md:col-start-2 md:row-span-full"
             alt={t("home.about.meImageAlt")}
+          />
+
+          <p
+            class="col-start-1 mb-12 md:h-screen"
+            dangerouslySetInnerHTML={t("about.intro")}
+          />
+          <p
+            class="col-start-1 mb-12 md:h-[10vh]"
+            dangerouslySetInnerHTML={t("about.tcc")}
+          />
+
+          <p
+            class="col-start-1 mb-12 md:mb-24"
+            dangerouslySetInnerHTML={t("about.jsp")}
           />
         </div>
 
-        <div class="auto-rows-fit z-10 col-span-2 flex min-h-screen flex-col items-center justify-center text-pretty bg-zinc-950 text-center">
+        <div class="col-span-full flex h-screen flex-col items-center justify-center text-center">
           <p
-            class="mb-4 max-w-xl text-lg tracking-tighter "
+            class="mb-4 max-w-md text-lg tracking-tighter"
             dangerouslySetInnerHTML={t("about.websites")}
           />
           <p
-            class="mb-12 max-w-xl text-2xl font-medium tracking-tighter text-white"
+            class="mb-12 max-w-md text-2xl font-medium tracking-tighter text-white lg:max-w-xl"
             dangerouslySetInnerHTML={t("about.sendMyMessage")}
           />
 
