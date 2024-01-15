094d393109c7ea1bb6ecb6c19198a4d70a454239
Abner Rodrigues <abnerluis1001@gmail.com>
2024-01-15 06:36:22 -0300

    feat(seo): add localized urls to head as alternates

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -1,10 +1,12 @@
 import { component$ } from "@builder.io/qwik";
 import { useDocumentHead, useLocation } from "@builder.io/qwik-city";
+import { useSpeakConfig } from "qwik-speak";
 
 /** The RouterHead component is placed inside of the document `<head>` element. */
 export const RouterHead = component$(() => {
   const head = useDocumentHead();
   const loc = useLocation();
+  const speakConfig = useSpeakConfig();
 
   return (
     <>
@@ -13,6 +15,17 @@ export const RouterHead = component$(() => {
       <link rel="canonical" href={loc.url.href} />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+
+      {speakConfig?.supportedLocales.map((locale) => {
+        return (
+          <link
+            key={locale.lang + "__alternate"}
+            rel="alternate"
+            hreflang={locale.lang}
+            href={`${loc.url.origin}${locale.lang === speakConfig.defaultLocale.lang ? "/" : `/${locale.lang}/`}`}
+          />
+        );
+      })}
 
       {head.meta.map((m) => (
         <meta key={m.key} {...m} />
