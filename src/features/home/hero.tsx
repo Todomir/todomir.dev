996eefc276d6ac95b50225f0a2ada1a9f8c246ee
Abner Rodrigues <abnerluis1001@gmail.com>
2024-02-05 15:37:00 -0300

    feat(hero): revamp hero animation

--- src/features/home/hero.tsx
+++ src/features/home/hero.tsx
@@ -1,10 +1,5 @@
-import {
-  $,
-  component$,
-  useOnWindow,
-  useSignal,
-  useVisibleTask$,
-} from "@builder.io/qwik";
+/* eslint-disable qwik/no-use-visible-task */
+import { component$, useSignal, useVisibleTask$ } from "@builder.io/qwik";
 import { animate, scroll, spring, stagger, timeline } from "motion";
 import { inlineTranslate } from "qwik-speak";
 import SplitType from "split-type";
@@ -21,114 +16,54 @@ export default component$(() => {
   const t = inlineTranslate();
 
   const asideRef = useSignal<HTMLDivElement>();
-  const userPrefences = useGetUserPreferences();
+  const userPreferences = useGetUserPreferences();
 
-  // We want this to run only once it is visible, and eagerly, on the client. So we use `useVisibleTask$`.
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(({ track }) => {
-    track(userPrefences);
+  useVisibleTask$(() => {
+    if (userPreferences.reducedMotion) return;
 
-    if (userPrefences.reducedMotion) return;
+    SplitType.create("#hero-title", {
+      types: "chars",
+      tagName: "span",
+      charClass: "split-target__char",
+    });
+    SplitType.create("#hero-subtitle", {
+      types: "words",
+      tagName: "span",
+      wordClass: "split-target__word",
+    });
 
-    scroll(
-      animate(
-        "#hero-content",
+    timeline([
+      ["#hero-title", { opacity: 1, y: 0 }],
+      ["#hero-subtitle", { opacity: 1, y: 0 }],
+      [
+        "#hero-title > span",
+        { opacity: [0, 1], rotateX: [-50, 0], y: [20, 0] },
+        { delay: stagger(0.02) },
+      ],
+      [
+        "#hero-subtitle > span",
+        { opacity: [0, 1], rotateX: [-50, 0], y: [20, 0] },
+        { delay: stagger(0.03), at: "-0.5" },
+      ],
+      [
+        "#hero .hero-image",
+        { x: ["40%", 0], opacity: 1 },
         {
-          opacity: [null, 0],
-          scale: [null, 0.95],
-          y: [null, "-500%"],
+          delay: stagger(0.2),
+          easing: spring({ damping: 50, stiffness: 100, mass: 10 }),
+          at: "-0.5",
         },
-        { easing: spring({ damping: 50, stiffness: 100, mass: 100 }) },
-      ),
-    );
-
-    const images = asideRef.value?.querySelectorAll("*");
-    if (!images) {
-      return;
-    }
-
-    for (const [index, el] of images.entries()) {
-      const isBottomImage = index >= 2;
-
-      const configs = {
-        animation: {
-          y: [null, isBottomImage ? "-2000%" : "-1000%"],
-        },
-        easing:
-          isBottomImage ?
-            spring({ damping: 50, stiffness: 100, mass: 300 })
-          : spring({ damping: 50, stiffness: 100, mass: 400 }),
-      };
-
-      scroll(animate(el, configs.animation, configs.easing));
-    }
+      ],
+      ["#hero-scroll", { opacity: 1, y: 0 }, { at: "-0.1" }],
+    ]);
   });
 
-  useOnWindow(
-    "DOMContentLoaded",
-    $(() => {
-      const aside = asideRef.value;
-      if (!aside) return;
-
-      const images = aside.querySelectorAll("img");
-      if (!images) return;
-
-      SplitType.create("#hero-subtitle", {
-        split: "words",
-        wordClass: "word opacity-0",
-        tagName: "span",
-      });
-
-      SplitType.create("#hero-title", {
-        split: "chars",
-        charClass: "char opacity-0",
-        tagName: "span",
-      });
-
-      timeline([
-        [
-          "#hero-title .char",
-          {
-            y: userPrefences.reducedMotion ? [null] : [10, 0],
-            rotateX: userPrefences.reducedMotion ? [null] : [90, 0],
-            opacity: 1,
-          },
-          {
-            delay: stagger(0.01),
-            easing: spring({ damping: 20, stiffness: 100, mass: 1 }),
-          },
-        ],
-        [
-          "#hero-subtitle .word",
-          {
-            y: userPrefences.reducedMotion ? [null] : [5, 0],
-            rotateX: userPrefences.reducedMotion ? [null] : [50, 0],
-            opacity: 1,
-          },
-          {
-            delay: userPrefences.reducedMotion ? 0 : stagger(0.05),
-            at: "-0.5",
-          },
-        ],
-        [
-          images,
-          { opacity: [0, 1], scale: [0.95, 1], x: ["-100%", 0] },
-          {
-            delay: userPrefences.reducedMotion ? 0 : stagger(0.1),
-            easing: spring({ damping: 50, stiffness: 100, mass: 10 }),
-            at: "-1.5",
-          },
-        ],
-        [
-          "#hero-scroll",
-          { opacity: [0, 1], y: [5, 0] },
-          {
-            at: "-1.5",
-          },
-        ],
-      ]);
-    }),
-  );
+  useVisibleTask$(() => {
+    if (userPreferences.reducedMotion) return;
+    scroll(
+      animate(".hero-image", { y: [null, "-1000%"] }, { easing: spring() }),
+    );
+  });
 
   return (
     <section
@@ -144,13 +79,13 @@ export default component$(() => {
           loading="eager"
           decoding="sync"
           alt={t("projects.kobraza_imoveis.description")}
-          class="absolute -left-24 top-2 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl"
+          class="hero-image absolute -left-24 top-2 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl"
         />
         <KdsThumb
           loading="eager"
           decoding="sync"
           alt={t("projects.kds_wahalla.description")}
-          class="absolute -right-24 -top-8 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl"
+          class="hero-image absolute -right-24 -top-8 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl"
         />
 
         {/* Bottom */}
@@ -158,13 +93,13 @@ export default component$(() => {
           loading="eager"
           decoding="sync"
           alt={t("projects.leonardo_nutrition.description")}
-          class="absolute -bottom-16 -right-36 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl @md/hero:-bottom-40"
+          class="hero-image absolute -bottom-16 -right-36 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl @md/hero:-bottom-40"
         />
         <LeonardoNutritionThumb
           loading="eager"
           decoding="sync"
           alt={t("projects.astromart.description")}
-          class="absolute -bottom-12 -left-32 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl @md/hero:-bottom-32 md:-left-24 lg:-left-8"
+          class="hero-image absolute -bottom-12 -left-32 aspect-[5/3] w-[clamp(15.625rem,7.1023rem+42.6136vw,34.375rem)] rounded-3xl bg-zinc-900 object-cover opacity-0 shadow-2xl @md/hero:-bottom-32 md:-left-24 lg:-left-8"
         />
       </aside>
 
@@ -174,21 +109,23 @@ export default component$(() => {
         class="my-auto py-24 @2xl/hero:py-36"
       >
         <Logo
-          id="hero-logo"
-          shouldFollowCursor
           shouldBlink
+          shouldFollowCursor
+          id="hero-logo"
           class="mx-auto animate-fade-in text-2xl text-white"
         />
 
         <h1
+          style={{ perspective: "1000px" }}
           id="hero-title"
-          class="split z-10 mt-12 animate-fade-in text-balance text-center text-5xl leading-normal tracking-tighter text-zinc-200 @md:text-6xl @md:leading-[1.2] @2xl/hero:text-7xl @2xl/hero:leading-normal"
+          class="split-target z-10 mt-12 text-balance text-center text-5xl leading-normal tracking-tighter text-zinc-200 opacity-0 @md:text-6xl @md:leading-[1.2] @2xl/hero:text-7xl @2xl/hero:leading-normal"
           dangerouslySetInnerHTML={t("home.hero.title")}
         />
 
         <h2
+          style={{ perspective: "1000px" }}
           id="hero-subtitle"
-          class="split mx-auto mt-10 max-w-[524px] animate-fade-in text-balance text-center text-xl leading-7"
+          class="split-target mx-auto mt-10 max-w-[524px] text-balance text-center text-xl leading-7 opacity-0"
         >
           {t("home.hero.subtitle")}
         </h2>
@@ -196,7 +133,7 @@ export default component$(() => {
         <div
           id="hero-scroll"
           aria-labelledby="scroll"
-          class="group mx-auto mt-20 flex h-fit w-fit animate-fade-in gap-4 rounded-full bg-zinc-900/10 p-9"
+          class="group mx-auto mt-20 flex h-fit w-fit animate-fade-in gap-4 rounded-full bg-zinc-900/10 p-9 opacity-0"
         >
           <IconArrowDown />
           <span id="scroll" class="sr-only text-zinc-200">
