8f5f43845f7e0ac68783bb0f1c459973d7a949af
Abner Rodrigues <abnerluis1001@gmail.com>
2024-02-29 19:29:37 -0300

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import type { Highlighter } from "shikiji";
-
 import { qwikCity } from "@builder.io/qwik-city/vite";
 import { qwikVite } from "@builder.io/qwik/optimizer";
 import { qwikSpeakInline } from "qwik-speak/inline";
-import rehypePrettyCode from "rehype-pretty-code";
-import { getHighlighter } from "shikiji";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { z } from "zod";
 
 import mdxCollections from "./plugins/collections";
 import { rewriteRoutes } from "./src/speak.routes";
-
-let highlighter: Highlighter;
-async function getOrCreateHighlighter() {
-  if (highlighter) return highlighter;
-  highlighter = await getHighlighter({ themes: ["vitesse-dark"] });
-  return highlighter;
-}
 
 export default defineConfig(() => {
   return {
@@ -43,42 +31,7 @@ export default defineConfig(() => {
           },
         ],
       }),
-      qwikCity({
-        rewriteRoutes,
-        mdxPlugins: {
-          rehypeAutolinkHeadings: true,
-          rehypeSyntaxHighlight: false,
-          remarkGfm: true,
-        },
-        mdx: {
-          rehypePlugins: [
-            [
-              () => rehypePrettyCode({ theme: "vitesse-dark" }) as any,
-              {
-                getHighlighter: getOrCreateHighlighter,
-                onVisitLine(node: any) {
-                  // Prevent lines from collapsing in `display: grid` mode, and allow empty
-                  // lines to be copy/pasted
-                  if (node.children.length === 0) {
-                    node.children = [{ type: "text", value: " " }];
-                  }
-                },
-                onVisitHighlightedLine(node: any) {
-                  // Each line node by default has `class="line"`.
-                  if (node.properties.className) {
-                    node.properties.className.push("line--highlighted");
-                  }
-                },
-                onVisitHighlightedWord(node: any) {
-                  if (node.properties.className) {
-                    node.properties.className = ["word--highlighted"];
-                  }
-                },
-              },
-            ],
-          ],
-        },
-      }),
+      qwikCity({ rewriteRoutes }),
       qwikVite(),
       qwikSpeakInline({
         supportedLangs: ["en", "pt-BR"],
