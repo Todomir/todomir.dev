b541c876fb84a90635c44911753002e55c4f2f18
Abner Rodrigues <abnerluis1001@gmail.com>
2025-04-27 18:55:45 -0300

    chore: remove unused global type declarations from layout

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -7,25 +7,6 @@ import Navbar from "~/components/navbar/navbar";
 import { UserPreferencesProvider } from "~/context/user-preferences";
 import { ONE_DAY_IN_SECONDS, ONE_MINUTE_IN_SECONDS } from "~/utils/constants";
 
-declare global {
-  interface Document {
-    startViewTransition?: (
-      updateCallback: () => Promise<void> | void,
-    ) => ViewTransition;
-  }
-
-  interface ViewTransition {
-    finished: Promise<void>;
-    ready: Promise<void>;
-    skipTransition: () => void;
-    updateCallbackDone: Promise<void>;
-  }
-
-  interface CSSStyleDeclaration {
-    viewTransitionName: string;
-  }
-}
-
 export const onGet: RequestHandler = async ({ cacheControl }) => {
   cacheControl({
     staleWhileRevalidate: ONE_DAY_IN_SECONDS,
